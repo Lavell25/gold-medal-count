43e105b4316984b577c3fbf1e4026e569fd284a3
Kahlil F <kahlilf@Kahlils-MacBook-Pro.local>
2022-01-15 11:40:17 -0600

    created Country datbase

--- app.py
+++ app.py
@@ -14,3 +14,16 @@ db = PostgresqlDatabase('project', user='postgres',
 class BaseModel(Model):
     class Meta:
         database = db
+
+
+class Country(BaseModel):
+    country_name = CharField()
+    gold_medal_count = IntegerField()
+    silver_medal_count = IntegerField()
+    bronze_medal_count = IntegerField()
+    total_medal_count = IntegerField()
+
+
+db.connect()
+db.drop_tables([Country])
+db.create_tables([Country])
